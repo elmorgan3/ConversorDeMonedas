289dbc80f9586f2d58a7ca5ab9393ef15e151a08
Morgan <morgan.cordoba@hotmail.com>
2016-10-13 12:54:49 +0200

    Coversot de monedas
    La app puede cambiar un valor a otro tipo de moneda

---
+++ Prac 4/Form4.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form4
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonFlecha = New System.Windows.Forms.Button()
+        Me.ButtonComa = New System.Windows.Forms.Button()
+        Me.TextBoxNum = New System.Windows.Forms.TextBox()
+        Me.ButtonC = New System.Windows.Forms.Button()
+        Me.ButtonConvertir = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ButtonCambiar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(117, 187)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(153, 187)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(30, 30)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(189, 187)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 30)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(117, 151)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(30, 30)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(153, 151)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(30, 30)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(189, 151)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(30, 30)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(117, 115)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(30, 30)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(153, 115)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(30, 30)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(189, 115)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(30, 30)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(117, 223)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(66, 30)
+        Me.Button0.TabIndex = 9
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonFlecha
+        '
+        Me.ButtonFlecha.Location = New System.Drawing.Point(171, 79)
+        Me.ButtonFlecha.Name = "ButtonFlecha"
+        Me.ButtonFlecha.Size = New System.Drawing.Size(48, 30)
+        Me.ButtonFlecha.TabIndex = 10
+        Me.ButtonFlecha.Text = "←"
+        Me.ButtonFlecha.UseVisualStyleBackColor = True
+        '
+        'ButtonComa
+        '
+        Me.ButtonComa.Location = New System.Drawing.Point(189, 223)
+        Me.ButtonComa.Name = "ButtonComa"
+        Me.ButtonComa.Size = New System.Drawing.Size(30, 30)
+        Me.ButtonComa.TabIndex = 11
+        Me.ButtonComa.Text = ","
+        Me.ButtonComa.UseVisualStyleBackColor = True
+        '
+        'TextBoxNum
+        '
+        Me.TextBoxNum.Location = New System.Drawing.Point(117, 26)
+        Me.TextBoxNum.Name = "TextBoxNum"
+        Me.TextBoxNum.ReadOnly = True
+        Me.TextBoxNum.Size = New System.Drawing.Size(102, 20)
+        Me.TextBoxNum.TabIndex = 12
+        Me.TextBoxNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'ButtonC
+        '
+        Me.ButtonC.Location = New System.Drawing.Point(117, 79)
+        Me.ButtonC.Name = "ButtonC"
+        Me.ButtonC.Size = New System.Drawing.Size(48, 30)
+        Me.ButtonC.TabIndex = 13
+        Me.ButtonC.Text = "C"
+        Me.ButtonC.UseVisualStyleBackColor = True
+        '
+        'ButtonConvertir
+        '
+        Me.ButtonConvertir.Location = New System.Drawing.Point(117, 259)
+        Me.ButtonConvertir.Name = "ButtonConvertir"
+        Me.ButtonConvertir.Size = New System.Drawing.Size(102, 30)
+        Me.ButtonConvertir.TabIndex = 14
+        Me.ButtonConvertir.Text = "Convertir"
+        Me.ButtonConvertir.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Dolares", "Yenes", "Libras", "Dragmes"})
+        Me.ComboBox1.Location = New System.Drawing.Point(117, 52)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(102, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'ButtonCambiar
+        '
+        Me.ButtonCambiar.Location = New System.Drawing.Point(225, 42)
+        Me.ButtonCambiar.Name = "ButtonCambiar"
+        Me.ButtonCambiar.Size = New System.Drawing.Size(102, 38)
+        Me.ButtonCambiar.TabIndex = 16
+        Me.ButtonCambiar.Text = "Cambiar valor de moneda"
+        Me.ButtonCambiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(371, 358)
+        Me.Controls.Add(Me.ButtonCambiar)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ButtonConvertir)
+        Me.Controls.Add(Me.ButtonC)
+        Me.Controls.Add(Me.TextBoxNum)
+        Me.Controls.Add(Me.ButtonComa)
+        Me.Controls.Add(Me.ButtonFlecha)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents ButtonFlecha As System.Windows.Forms.Button
+    Friend WithEvents ButtonComa As System.Windows.Forms.Button
+    Friend WithEvents ButtonC As System.Windows.Forms.Button
+    Friend WithEvents ButtonConvertir As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Public WithEvents TextBoxNum As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonCambiar As System.Windows.Forms.Button
+
+End Class
---
+++ Prac 4/Form4.vb
@@ -0,0 +1,241 @@
+﻿Public Class Form4
+    Dim euros As Double
+    Dim dolares As Double
+    Dim yenes As Double
+    Dim libras As Double
+    Dim dragmes As Double
+    Dim operacion As Double
+    Dim primeraVezDol As Boolean = False
+    Dim primeraVezYen As Boolean = False
+    Dim primeraVezLib As Boolean = False
+    Dim primeraVezDra As Boolean = False
+    Dim primeraComa As Boolean = True
+    Dim coma As Boolean = False
+    Dim contDecimal As Integer = 0
+
+
+    Private Sub ButtonConvertir_Click(sender As Object, e As EventArgs) Handles ButtonConvertir.Click
+
+        Try
+            Select Case ComboBox1.Text
+                Case "Dolares"
+                    If primeraVezDol = False Then
+
+                        dolares = InputBox("¿A cuanto esta el dolar?")
+                        primeraVezDol = True
+                        operacion = CDbl(TextBoxNum.Text) * dolares
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    Else
+                        operacion = CDbl(TextBoxNum.Text) * dolares
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    End If
+
+                Case "Yenes"
+                    If primeraVezYen = False Then
+                        yenes = InputBox("¿A cuanto esta el yen?")
+                        primeraVezDol = True
+                        operacion = CDbl(TextBoxNum.Text) * yenes
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    Else
+                        operacion = CDbl(TextBoxNum.Text) * yenes
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    End If
+
+                Case "Libras"
+                    If primeraVezDol = False Then
+                        libras = InputBox("¿A cuanto esta la libra?")
+                        primeraVezDol = True
+                        operacion = CDbl(TextBoxNum.Text) * libras
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    Else
+                        operacion = CDbl(TextBoxNum.Text) * libras
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    End If
+
+                Case "Dragmes"
+                    If primeraVezDra = False Then
+                        dragmes = InputBox("¿A cuanto esta el dragma?")
+                        primeraVezDol = True
+                        operacion = CDbl(TextBoxNum.Text) * dragmes
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    Else
+                        operacion = CDbl(TextBoxNum.Text) * dragmes
+                        TextBoxNum.Text = Format(operacion, "0.00")
+
+                    End If
+            End Select
+        Catch
+            MsgBox("Introduce un valor que se pueda convertir.")
+        End Try
+
+    End Sub
+
+    Private Sub ButtonC_Click(sender As Object, e As EventArgs) Handles ButtonC.Click
+        TextBoxNum.Text = ""
+        coma = False
+        contDecimal = 0
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(1)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(1)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(2)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(2)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(3)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(3)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(4)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(4)
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(5)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(5)
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(6)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(6)
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(7)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(7)
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(8)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(8)
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(9)
+        End If
+        If coma = False And contDecimal <= 2 Then
+            TextBoxNum.Text = TextBoxNum.Text + CStr(9)
+        End If
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        If coma = True And contDecimal <= 1 Then
+            contDecimal = contDecimal + 1
+            TextBoxNum.Text = TextBoxNum.Text + CStr(0)
+        End If
+
+    End Sub
+
+    Private Sub ButtonFlecha_Click(sender As Object, e As EventArgs) Handles ButtonFlecha.Click
+        Try
+            If TextBoxNum.Text.Last = "," Then
+                coma = False
+                contDecimal = 0
+            End If
+
+            If coma = True Then
+                contDecimal = contDecimal - 1
+            End If
+
+            TextBoxNum.Text = Mid(TextBoxNum.Text, 1, Len(TextBoxNum.Text) - 1)
+        Catch
+
+        End Try
+    End Sub
+
+    Private Sub ButtonComa_Click(sender As Object, e As EventArgs) Handles ButtonComa.Click
+
+
+        If (coma = False And TextBoxNum.Text <> "") Then
+            TextBoxNum.Text = TextBoxNum.Text + ","
+
+            coma = True
+        End If
+
+    End Sub
+
+    Private Sub ButtonCambiar_Click(sender As Object, e As EventArgs) Handles ButtonCambiar.Click
+        Try
+            Select Case ComboBox1.Text
+                Case "Dolares"
+                    dolares = InputBox("¿A cuanto esta el dolar?")
+                    operacion = CDbl(TextBoxNum.Text) * dolares
+                    TextBoxNum.Text = Format(operacion, "0.00")
+
+                Case "Yenes"
+                    yenes = InputBox("¿A cuanto esta el yen?")
+                    operacion = CDbl(TextBoxNum.Text) * yenes
+                    TextBoxNum.Text = Format(operacion, "0.00")
+
+                Case "Libras"
+                    libras = InputBox("¿A cuanto esta la libra?")
+                    operacion = CDbl(TextBoxNum.Text) * libras
+                    TextBoxNum.Text = Format(operacion, "0.00")
+
+                Case "Dragmes"
+                    dragmes = InputBox("¿A cuanto esta el dragma?")
+                    operacion = CDbl(TextBoxNum.Text) * dragmes
+                    TextBoxNum.Text = Format(operacion, "0.00")
+
+            End Select
+        Catch
+            MsgBox("Introduce un valor que se pueda convertir.")
+        End Try
+    End Sub
+End Class
